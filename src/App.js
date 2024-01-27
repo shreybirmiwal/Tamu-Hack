a9f8297a74337a53001cbe9e74fd46f35764cb6a
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-01-27 16:10:12 -0600

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import UserMVP from "./Pages/UserMVP";
+
 import SeatingChart from "./Pages/SeatingChart";
 import { Landing } from "./Pages/Landing";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom" 
 
 function App() {
   return (
     <div className="max-h-screen">
-      <Landing/>
+
+      <Router>
+        <Routes>
+          
+          <Route exact path="/" element={<Landing />}/>
+          <Route path="/chart" element={<SeatingChart />}/>
+
+        </Routes>
+      </Router>
+
     </div>
   );
 }
