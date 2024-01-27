15d7f399677c0cf158c1151a7d58f454fc6fac45
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-01-27 14:06:11 -0600

    plane layout

--- src/Pages/UserMVP.js
+++ src/Pages/UserMVP.js
@@ -1,20 +1,42 @@
 import React from 'react'
 import SeatCard from '../Components/SeatCard';
 
-const seatList = [
-    { seatNumber: '1A', isTaken: true },
-    { seatNumber: '1B', isTaken: false },
-    // Add more seat data as needed
-  ];
+const seatList = [];
 
+for (let row = 1; row <= 20; row++) {
+    seatList.push(
+      { row: row, seatNumber: `${row}A`, isTaken: false, type: 'window', seat: row*6+1 },
+      { row: row, seatNumber: `${row}B`, isTaken: false, type: 'middle', seat: row*6+2  },
+      { row: row, seatNumber: `${row}C`, isTaken: false, type: 'aisle', seat: row*6+3  },
+      { row: row, seatNumber: `${row}D`, isTaken: false, type: 'aisle', seat: row*6+4 },
+      { row: row, seatNumber: `${row}E`, isTaken: false, type: 'middle', seat: row*6+5  },
+      { row: row, seatNumber: `${row}F`, isTaken: false, type: 'window',seat: row*6+6  }
+    );
+  }
 
 function UserMVP() {
   return (
+
     <div>
         {seatList.map((seat, index) => (
-            <SeatCard key={index} seatNumber={seat.seatNumber} isTaken={seat.isTaken} />
+            ((index+1) % 3 === 0 && (index+1) % 6 !== 0) ? (
+            <>
+                <SeatCard key={index} seatNumber={seat.seatNumber} isTaken={seat.isTaken} />
+                EWMPTY SEAT
+            </>
+            ) : (
+                ((index+1) % 3 === 0 && (index+1) % 6 == 0) ? (
+                    <>
+                <SeatCard key={index} seatNumber={seat.seatNumber} isTaken={seat.isTaken} />
+                NEXT ROW
+                </>
+            ) : (
+                <SeatCard key={index} seatNumber={seat.seatNumber} isTaken={seat.isTaken} />
+            )
+            )
         ))}
     </div>
+
   )
 }
 
